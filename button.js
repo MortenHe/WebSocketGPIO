392b19d700c6909b6f90236a853eb953da43492f
Martin Helfer <martin-helfer@gmx.de>
2018-05-09 22:27:31 +0200

    LED

--- button.js
+++ button.js
@@ -4,16 +4,18 @@ const WebSocket = require('ws');
 //Mit WebsocketServer verbinden
 const ws = new WebSocket('ws://localhost:8080');
 
+//GPIO Bibliothek laden
 const Gpio = require('onoff').Gpio;
-const led = new Gpio(17, 'out');
 
 //Button soll auf steigenden Wert reagieren (=Knopfdruck)
 const buttonNext = new Gpio(4, 'in', 'rising', { debounceTimeout: 10 });
+//Buttons 4, 14, 15
 
-//4, 14, 15
-console.log(led.readSync());
+//LED anlegen
+const led = new Gpio(21, 'out');
+//LED 16, 20, 21
 
-//Mit WSS verbinden
+//Wenn Verbindung mit WSS hergestellt wird
 ws.on('open', function open() {
     console.log("logged")
 
@@ -21,6 +23,7 @@ ws.on('open', function open() {
     buttonNext.watch(function (err, value) {
         console.log("next track");
 
+        //LED an fur 0,5 sek
         led.writeSync(1);
         setTimeout(function () {
             led.writeSync(0);
