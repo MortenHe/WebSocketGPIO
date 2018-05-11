8f9213239b105de9fdbfb792f3f19e8a1747d6cf
Martin Helfer <martin-helfer@gmx.de>
2018-05-11 11:18:22 +0200

    start-rfid-playlist

--- rfid.js
+++ rfid.js
@@ -1,14 +1,31 @@
+//Mit WebsocketServer verbinden
+const WebSocket = require('ws');
+const ws = new WebSocket('ws://localhost:8080');
+
 //RFID Bibliothek laden
-var rfid = require('node-rfid');
+//var rfid = require('node-rfid');
 
-//RFID-Karte lesen
-rfid.read(function (err, result) {
 
-    //Fehler?
-    if (err) {
-        console.log("Sorry, some hardware error occurred");
-    }
+//Wenn Verbindung mit WSS hergestellt wird
+ws.on('open', function open() {
+    console.log("connected to wss");
 
-    //print rfid tag UID
-    console.log(result);
+    //RFID-Karte lesen
+    /*
+    rfid.read(function (err, result) {
+    
+        //Fehler?
+        if (err) {
+            console.log("Sorry, some hardware error occurred");
+        }
+    
+        //print rfid tag UID
+        console.log(result);
+    });*/
+
+    //Nachricht an WSS schicken
+    ws.send(JSON.stringify({
+        type: "set-rfid-playlist",
+        value: 278820847058
+    }));
 });
